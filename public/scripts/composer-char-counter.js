ab04b89d1826855026d029f2431bab693ea0d233
cytusalive <cytusomega@gmail.com>
2021-07-14 08:09:51 +0000

    make textarea auto extend when full

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ const maxChars = 140;
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
     let input = $("#tweet-text").val();
-    console.log(input);
     let charCount = maxChars - input.length;
     let counter = $(this).siblings("#underTextArea").children(".counter");
     counter.text(charCount);
@@ -11,6 +10,10 @@ $(document).ready(function() {
       counter.css('color', '#FF0000');
     } else {
       counter.css('color', '#545149')
+    }
+    
+    if (this.scrollTop > 0) {
+      $(this).css('height', '+=1.5');
     }
   })
 });
