382b7c6aebfa2ca2e04b97b3e169e3a9a4e5d9e0
cytusalive <cytusomega@gmail.com>
2021-07-14 07:26:56 +0000

    working char counter with colour change when full

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,16 @@
+const maxChars = 140;
+
 $(document).ready(function() {
-  // --- our code goes here ---
-  console.log("DOM ready");
+  $("#tweet-text").on('input', function() {
+    let input = $("#tweet-text").val();
+    console.log(input);
+    let charCount = maxChars - input.length;
+    let counter = $(this).siblings("#underTextArea").children(".counter");
+    counter.text(charCount);
+    if (charCount < 0) {
+      counter.css('color', '#FF0000');
+    } else {
+      counter.css('color', '#545149')
+    }
+  })
 });
