b6fd31f322991a58ecd279cecb56dc74962ab45e
cytusalive <cytusomega@gmail.com>
2021-07-16 05:59:32 +0000

    adding focus on textbox functionality

--- public/scripts/scroll-to-textarea.js
+++ public/scripts/scroll-to-textarea.js
@@ -1,6 +1,7 @@
-// Scrolls to top when clicked
+// Scrolls to top then focuses on textbox when clicked
 $(document).ready(() => {
   $(".new").on("click", () => {
     window.scrollTo(0, 0);
+    $("#tweet-text").focus();
   })
 })
